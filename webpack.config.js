cfcca9ed21fceb4c9bc0f21233d8f0f30029e411
Alan Privet <alannprivet@gmail.com>
2020-08-15 19:55:34 +0300

    Adopted tailwindcss over bootstrap utilities

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,73 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-// const CleanWebpackPlugin = require('clean-webpack-plugin')
+const TerserJSPlugin = require('terser-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const isProduction = (process.env.NODE_ENV === 'production')
 
-module.exports = {
-  mode: 'development',
-  entry: {
-    ds: './ds.sass',
-    'ds-utilities': './ds-utilities.sass'
-  },
-  output: {
-    path: path.resolve(__dirname, 'css'),
-    filename: '[name].bundle.js'
-  },
-  devtool: isProduction ? false : 'inline-source-map',
-  devServer: {
-    contentBase: './css',
-    stats: {
-      preset: 'minimal',
-      assets: true,
-      modules: false,
-      chunks: false,
-      chunkModules: false
-    }
-  },
-  module: {
-    rules: [{
-      test: /\.(sa|sc|c)ss$/,
-      use: [
-        {
-          loader: MiniCssExtractPlugin.loader,
-          options: {
-            hmr: !isProduction,
+module.exports = ({ prod = false } = {}) => {
+  return {
+    mode: prod ? 'production' : 'development',
+    entry: {
+      ds: './ds.sass'
+    },
+    output: {
+      path: path.resolve(__dirname, 'css'),
+      // filename: '[name].css'
+    },
+    devtool: prod ? false : 'inline-source-map',
+    devServer: {
+      contentBase: './css',
+      stats: {
+        preset: 'minimal',
+        assets: true,
+        modules: false,
+        chunks: false,
+        chunkModules: false
+      }
+    },
+    optimization: {
+      minimizer: [new TerserJSPlugin({})],
+    },
+    module: {
+      rules: [{
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: !prod,
+            },
           },
-        },
-        {
-          loader: 'css-loader'
-        },
-        {
-          loader: 'sass-loader',
-          options: {
-            sassOptions: {}
+          { loader: 'css-loader' },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: !prod
+            }
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: !prod,
+              sassOptions: {}
+            }
           }
-        }
-      ]
-    }]
-  },
-  plugins: [
-    // new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    }),
-    ...isProduction
-    ? []
-    : [
-        new HtmlWebpackPlugin({
-          template: 'index.html'
-        })
-      ]
-  ]
+        ]
+      }]
+    },
+    plugins: [
+      // new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+      new MiniCssExtractPlugin({
+        filename: '[name].css'
+      }),
+      ...prod
+        ? []
+        : [
+          new HtmlWebpackPlugin({
+            template: 'index.html'
+          })
+        ]
+    ]
+  }
 };
